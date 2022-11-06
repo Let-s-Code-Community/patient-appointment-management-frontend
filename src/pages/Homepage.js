ad9bb30bbb335c72d48f02107966ceb45892884f
Majedul Hasan <hasanmajedul@gmail.com>
2022-11-07 01:54:13 +0600

    jh

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import Services from '../components/homePageComponents/Services';
-import CollapseItems from '../components/homePageComponents/CollapseItems/CollapseItems';
-import SectionHeader from '../components/SectionHeader';
-import SectionIntro from '../components/SectionIntro';
-import SectionSubHeader from '../components/SectionSubHeader';
 import Navbar from '../components/shared/navbar/Navbar';
 import Header from './../components/Header/Header';
 
@@ -12,13 +8,10 @@ const Homepage = () => {
   return (
     <React.Fragment>
      
-      <Services />
+     
     <Navbar />
-      <Header />
-      <CollapseItems />
-      <SectionSubHeader />
-      <SectionHeader />
-      <SectionIntro />
+      <Header />     
+      <Services />
     </React.Fragment>
   )
 }
